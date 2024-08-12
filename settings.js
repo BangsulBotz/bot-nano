4e90fbb7d5afe9f355bd11954898c65ac14bfda1
BangsulBotz <bangsulstart1@gmail.com>
2024-08-12 17:37:08 +0700

    Initial commit

---
+++ settings.js
@@ -0,0 +1,147 @@
+const chalk = require("chalk")
+const fs = require("fs")
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = true //auto block 212 (true to on, false to off)
+global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.antispam = false//auto kick spammer (true to on, false to off)
+//===============SETTING MENU==================\\
+global.thumbnail = fs.readFileSync("./data/image/thumb.jpg")
+global.ig = '@mhd_ghfar_'
+global.yt = 'bangsulstart'
+global.ttowner = '@bangsulstart'
+global.ownername = 'BANGSULSTART'
+global.owner = ['6282268881337'] // SETTING JUGA DI FOLDER DATABASE 
+global.ownernomer = '6282268881337'
+global.socialm = 'GitHub: -'
+global.location = 'Indonesia' 
+//========================setting Payment=====================\\
+global.nodana = '6282268881337' // KOSONG KAN JIKA TIDAK ADA
+global.nogopay = '6282268881337' // KOSONG KAN JIKA TIDAK ADA 
+global.noovo = '6282268881337' // KOSONG KAN JIKA TIDAK ADA
+//==================setting Payment Name===========================\\
+global.andana = 'muhammad ghofar' // KOSONG KAN JIKA TIDAK ADA
+global.angopay = 'muhammad ghofar' // KOSONG KAN JIKA TIDAK ADA
+global.anovo = 'nama ovo mu' // KOSONG KAN JIKA TIDAK ADA
+//==================setting bot===========================\\
+global.botname = "BangsulBotz ID"
+global.ownernumber = '6282268881337'
+global.botnumber = '6281266021317'
+global.ownername = 'BANGSULSTART'
+global.ownerNumber = ["6282268881337@s.whatsapp.net"]
+global.ownerweb = ""
+global.websitex = ""
+global.wagc = "https://wa.me/+6282268881337"
+global.saluran = "https://wa.me/+6282268881337"
+global.themeemoji = '🪀'
+global.wm = "≧﹏≦"
+global.botscript = 'Dah gede nyari sc 🗿' //script link
+global.packname = "Sticker By"
+global.author = "\nBOT WA"
+global.creator = "6282268881337@s.whatsapp.net"
+//======================== CPANEL FITUR ===========================\\
+global.domain = '-' // Isi Domain Lu jangan kasih tanda / di akhir link
+global.apikey = '-' // Isi Apikey Plta Lu
+global.capikey = '-' // Isi Apikey Pltc Lu
+//=========================================================//
+//Server create panel egg pm2
+global.apikey2 = '-' // Isi Apikey Plta Lu
+global.capikey2 = '-' // Isi Apikey Pltc Lu
+global.domain2 = '-' // Isi Domain Lu
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //jangan di ubah
+
+global.eggsnya2 = '15' // id eggs yang dipakai
+global.location2 = '1' // id location
+//===========================//
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // id eggs yang dipakai
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+//===========================//
+
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+//new
+global.prefix = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+wait: "*Tunggu sebentar 🗿☕*",
+   success: "_ini yang mulia..._",
+   on: "Sudah Aktif", 
+   off: "Sudah Off",
+   query: {
+       text: "*mana teks nya bro??*",
+       link: "*mana link nya bro??*",
+   },
+   error: {
+       fitur: "Mohon Maaf Kak Fitur Eror Silahkan Chat Developer Bot Agar Bisa Segera Diperbaiki",
+   },
+   only: {
+       group: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Di Dalam *Group*",
+       private: "Maaf Kak Fitur Ini Hanya Bisa Digunakan Di Dalam *Private Chat*",
+       owner: "Maaf Kak Fitur Ini Hanya Bisa Digunakan oleh *Owner Bot*",
+       admin: "Maaf Kak Fitur Ini Hanya Bisa Digunakan oleh *admin Bot*",
+       badmin: "Maaf Kak Kaya Nya Kakak Tidak Bisa Menggunakan Fitur Ini Di Karenakan Bot Bukan Admin Group",
+       premium: "lu itu user gratisan tolol. Dasar idiot!! \nminat? chat owner\nketik:  .ᴏᴡɴᴇʀ",
+   }
+}
+ 
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB"
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
